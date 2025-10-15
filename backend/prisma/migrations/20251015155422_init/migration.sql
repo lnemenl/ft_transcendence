2ab19cd0bac5d09aa07ba85dd42ddba1f7400c5a
Milad <milad.rahmat@gmail.com>
2025-10-15 19:01:24 +0300

    Applied the changes made from schema.prisma to prisma client, generatesd a new migration.sql file, Changed User id from number to string and using cuid() so that the id is actually unique on a scalable level

---
+++ backend/prisma/migrations/20251015155422_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isTwoFactorEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "twoFactorSecret" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "winnerId" TEXT NOT NULL,
+    "tournamentId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Game_winnerId_fkey" FOREIGN KEY ("winnerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Game_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Tournament" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "startDate" DATETIME NOT NULL,
+    "endDate" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "TournamentParticipant" (
+    "userId" TEXT NOT NULL,
+    "tournamentId" TEXT NOT NULL,
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "isEliminated" BOOLEAN NOT NULL DEFAULT false,
+    "rank" INTEGER,
+
+    PRIMARY KEY ("userId", "tournamentId"),
+    CONSTRAINT "TournamentParticipant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "TournamentParticipant_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_UserGames" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_UserGames_A_fkey" FOREIGN KEY ("A") REFERENCES "Game" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_UserGames_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserGames_AB_unique" ON "_UserGames"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserGames_B_index" ON "_UserGames"("B");
