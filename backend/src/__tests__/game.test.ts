7f519ea12f21dcc5275f4883665ded1d0ffb7fd1
Milad <milad.rahmat@gmail.com>
2025-10-17 20:03:23 +0300

    Implemented second user login for game, implemented endpoint to create a Game, added tests

---
+++ backend/src/__tests__/game.test.ts
@@ -0,0 +1,169 @@
+import request from "supertest";
+import app from "../index";
+import { prisma } from "../utils/prisma";
+import { createGame } from "../services/game.service";
+
+const testUser1 = {
+  email: "ci_test_game@example.com",
+  password: "Password123!",
+};
+const testUSer2 = {
+  email: "ci_test1_game@example.com",
+  password: "Password123!",
+};
+
+beforeAll(async () => {
+  process.env.NODE_ENV = "test";
+  await app.ready();
+  await prisma.$connect();
+  await prisma.user.deleteMany({});
+  await prisma.game.deleteMany({});
+});
+
+afterAll(async () => {
+  await prisma.user.deleteMany({});
+  await app.close();
+  await prisma.$disconnect();
+});
+
+describe("Game tests", () => {
+  let cookie1: string;
+  let cookie2: string;
+  let cookie: string[] = [];
+  let playerToken: string | undefined;
+  let testUser1Id: string;
+  let testUser2Id: string;
+
+  it("Registering both users", async () => {
+    const res1 = await request(app.server)
+      .post("/api/register")
+      .send(testUser1)
+      .expect(201);
+    const res2 = await request(app.server)
+      .post("/api/register")
+      .send(testUSer2)
+      .expect(201);
+
+    testUser1Id = res1.body.id;
+    testUser2Id = res2.body.id;
+    expect(testUser1Id).toBeDefined();
+    expect(testUser2Id).toBeDefined();
+  });
+
+  it("Logging in the main user", async () => {
+    const res = await request(app.server).post("/api/login").send(testUser1);
+
+    expect(res.status).toBe(200);
+    cookie1 = res.headers["set-cookie"];
+    expect(cookie).toBeDefined();
+    expect(res.body).toHaveProperty("accessToken");
+  });
+
+  it("Logging in the player", async () => {
+    const res = await request(app.server)
+      .post("/api/login/player2")
+      .set("Cookie", cookie1)
+      .send(testUSer2);
+
+    expect(res.status).toBe(200);
+    cookie2 = res.headers["set-cookie"];
+    expect(cookie2).toBeDefined();
+    const cookieString = cookie2.find((c: string) =>
+      c.startsWith("player2_token="),
+    );
+    playerToken = cookieString?.split(";")[0].split("=")[1];
+    expect(playerToken).toBeDefined();
+  });
+
+  it("POST /games with valid credentials should pass", async () => {
+    const requestBody = { winnerId: testUser1Id, tournamentId: undefined };
+    cookie = [...cookie1, ...cookie2];
+    const res = await request(app.server)
+      .post("/api/games")
+      .set("Cookie", cookie.join("; "))
+      .send(requestBody);
+
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty(
+      "id",
+      "winnerId",
+      "winner",
+      "players",
+      "tounamentId",
+      "tournament",
+      "createdAt",
+    );
+  });
+
+  it("POST /games with no winnerId should fail", async () => {
+    const requestBody = { winnerId: undefined, tournamentId: undefined };
+    const res = await request(app.server)
+      .post("/api/games")
+      .set("Cookie", cookie)
+      .send(requestBody);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("error", "Bad Request");
+  });
+
+  it("POST /games with no token for player 2 should pass", async () => {
+    const requestBody = { winnerId: testUser1Id, tournamentId: undefined };
+    const res = await request(app.server)
+      .post("/api/games")
+      .set("Cookie", cookie1)
+      .send(requestBody);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("ok", true);
+  });
+
+  it("POST /games with an expired token for player 2 should fail", async () => {
+    const token = app.jwt.sign({ sub: testUser2Id! }, { expiresIn: "1s" });
+
+    await new Promise((resolve) => setTimeout(resolve, 1100));
+
+    const badCookie = [...cookie1, `player2_token=${token}`];
+    const requestBody = { winnerId: testUser1Id, tournamentId: undefined };
+    const res = await request(app.server)
+      .post("/api/games")
+      .set("Cookie", badCookie)
+      .send(requestBody);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("error", "Player 2 Unauthorized");
+  });
+
+  it("POST /games with invalid winnerId should fail", async () => {
+    const requestBody = { winnerId: "123", tournamentId: undefined };
+    const res = await request(app.server)
+      .post("/api/games")
+      .set("Cookie", cookie.join("; "))
+      .send(requestBody);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toHaveProperty("error", "Invalid ID");
+  });
+
+  it("Testing the last else branch of catch in createGame()", async () => {
+    const spy = jest.spyOn(prisma.game, "create").mockImplementation(() => {
+      throw new Error("Some unexpected error");
+    });
+
+    await expect(
+      createGame("winnerId", "player1Id", "player2Id", "tournamentId"),
+    ).rejects.toThrow("Bad request");
+
+    spy.mockRestore();
+  });
+
+  it("POST /games with invalid tournamentId should fail", async () => {
+    const requestBody = { winnerId: testUser1Id, tournamentId: "123" };
+    const res = await request(app.server)
+      .post("/api/games")
+      .set("Cookie", cookie.join("; "))
+      .send(requestBody);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toHaveProperty("error", "Invalid ID");
+  });
+});
--- backend/src/routes/game.ts
+++ backend/src/routes/game.ts
@@ -1,18 +1,43 @@
 import { FastifyInstance } from "fastify";
+import { createGame } from "../services/game.service";
 import schemas from "./schema.json";
 
 const gameRoutes = async (fastify: FastifyInstance) => {
   fastify.post(
     "/games",
-    { schema: schemas.newGame },
+    {
+      schema: schemas.newGame,
+      preHandler: [fastify.authenticate],
+    },
     async (request, reply) => {
+      let player: unknown;
+      const { player2_token } = request.cookies;
+      if (player2_token) {
+        try {
+          player = fastify.jwt.verify(player2_token);
+        } catch (_err) {
+          return reply.status(400).send({ error: "Player 2 Unauthorized" });
+        }
+      } else {
+        return reply.status(200).send({ ok: true });
+      }
       try {
-        const { winnerId, player1Id, player2Id, tournamentId } = request.body;
+        const player1 = (request.user as { sub: string }).sub;
+        const { winnerId, tournamentId } = request.body as {
+          winnerId: string;
+          tournamentId: string;
+        };
+        const player2 = (player as { sub: string }).sub;
+        const game = await createGame(winnerId, player1, player2, tournamentId);
+        return reply.status(201).send(game);
       } catch (err) {
+        if ((err as Error).message === "Invalid ID") {
+          return reply.status(404).send({ error: (err as Error).message });
+        }
         return reply.status(400).send({ error: (err as Error).message });
       }
     },
   );
-}
+};
 
 export default gameRoutes;
