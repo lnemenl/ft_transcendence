35bd6a198ae95aadb80fc29bf0eed7a4e7cb0476
Milad <milad.rahmat@gmail.com>
2025-11-07 19:53:09 +0200

    Added an endpoint for ending a tournament and declaring a winner

--- backend/src/routes/tournament.ts
+++ backend/src/routes/tournament.ts
@@ -1,8 +1,49 @@
 import { FastifyInstance } from 'fastify';
 import schemas from './schema.json';
 import { prisma } from '../utils/prisma';
+import { Prisma } from '@prisma/client';
 import { createTournament } from '../services/tournament.service';
 import { createGame } from '../services/game.service';
+
+const tournamentSelect = {
+  id: true,
+  winner: {
+    select: {
+      id: true,
+      username: true,
+      avatarUrl: true,
+    },
+  },
+  participants: {
+    select: {
+      id: true,
+      username: true,
+      avatarUrl: true,
+    },
+  },
+  games: {
+    select: {
+      id: true,
+      winner: {
+        select: {
+          id: true,
+          username: true,
+          avatarUrl: true,
+        },
+      },
+      players: {
+        select: {
+          id: true,
+          username: true,
+          avatarUrl: true,
+        },
+      },
+      createdAt: true,
+    },
+  },
+  startDate: true,
+  endDate: true,
+};
 
 const tournamentRoutes = async (fastify: FastifyInstance) => {
   fastify.post(
@@ -13,7 +54,7 @@ const tournamentRoutes = async (fastify: FastifyInstance) => {
     },
     async (request, reply) => {
       const { participants } = request.body as { participants: string[] };
-      if (!participants || participants.length < 2) {
+      if (participants.length < 2) {
         return reply.status(400).send({ error: 'Invalid number of participants' });
       }
 
@@ -42,10 +83,7 @@ const tournamentRoutes = async (fastify: FastifyInstance) => {
           players: string[];
           tournamentId: string;
         };
-        if (!tournamentId) {
-          return reply.status(400).send({ error: 'No tournament ID provided' });
-        }
-        if (!players || players.length !== 2) {
+        if (players.length !== 2) {
           return reply.status(400).send({ error: 'Invalid players' });
         }
         const game = await createGame(winner, players[0], players[1], tournamentId);
@@ -53,7 +91,8 @@ const tournamentRoutes = async (fastify: FastifyInstance) => {
       } catch (err) {
         if ((err as Error).message === 'Invalid ID') {
           reply.status(404).send({ error: (err as Error).message });
-        } else if ((err as Error).message === 'Internal server error') {
+        }
+        if ((err as Error).message === 'Internal server error') {
           return reply.status(500).send({ error: (err as Error).message });
         }
         return reply.status(400).send({ error: (err as Error).message });
@@ -72,53 +111,43 @@ const tournamentRoutes = async (fastify: FastifyInstance) => {
         const { id } = request.params as { id: string };
         const tournament = await prisma.tournament.findUnique({
           where: { id },
-          select: {
-            id: true,
-            winner: {
-              select: {
-                id: true,
-                username: true,
-                avatarUrl: true,
-              },
-            },
-            participants: {
-              select: {
-                id: true,
-                username: true,
-                avatarUrl: true,
-              },
-            },
-            games: {
-              select: {
-                id: true,
-                winner: {
-                  select: {
-                    id: true,
-                    username: true,
-                    avatarUrl: true,
-                  },
-                },
-                players: {
-                  select: {
-                    id: true,
-                    username: true,
-                    avatarUrl: true,
-                  },
-                },
-                createdAt: true,
-              },
-            },
-            startDate: true,
-            endDate: true,
-          },
+          select: tournamentSelect,
         });
 
-        if (!tournament) {
-          return reply.status(400).send({ error: 'Invalid tournament id' });
-        }
         return reply.status(200).send(tournament);
       } catch (err) {
         return reply.status(500).send({ error: (err as Error).message });
+      }
+    },
+  );
+
+  fastify.put(
+    '/tournament/:id',
+    {
+      schema: schemas.updateTournamentById,
+      preHandler: [fastify.authenticate],
+    },
+    async (request, reply) => {
+      try {
+        const { winner } = request.body as { winner: string };
+        const { id } = request.params as { id: string };
+
+        const tournament = await prisma.tournament.update({
+          where: { id },
+          data: {
+            winner: {
+              connect: { id: winner },
+            },
+            endDate: new Date(),
+          },
+          select: tournamentSelect,
+        });
+        return reply.status(200).send(tournament);
+      } catch (err) {
+        if (err instanceof Prisma.PrismaClientKnownRequestError) {
+          reply.status(404).send({ error: 'Invalid winner' });
+        }
+        reply.status(500).send({ error: 'Internal server error' });
       }
     },
   );
--- backend/src/services/tournament.service.ts
+++ backend/src/services/tournament.service.ts
@@ -4,14 +4,20 @@ import { Prisma } from '@prisma/client';
 export const createTournament = async (participants: string[]) => {
   try {
     const players = participants.map((participant) => ({ id: participant }));
-    const tournament = await prisma.tournament.create({ data: { participants: { connect: players } } });
-    console.log(tournament);
-    return tournament.id;
+    const { id } = await prisma.tournament.create({
+      data: {
+        participants: {
+          connect: players,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+    return id;
   } catch (err) {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
-      if (err.code === 'P2025') {
-        throw new Error('Invalid ID');
-      }
+      throw new Error('Invalid ID');
     }
     throw new Error('Internal server error');
   }
