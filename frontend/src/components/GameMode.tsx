5df8bc4c994ca6624fa2bb6e3b1ae8fc7c22c144
Colin <cpireyre@student.42.fr>
2025-11-14 14:04:55 +0200

    Add multilingual text

--- frontend/src/components/GameMode.tsx
+++ frontend/src/components/GameMode.tsx
@@ -1,4 +1,6 @@
 import { useGame } from "./GameContext";
+import { t } from "./lang";
+import { useLanguage } from "./useLanguage";
 
 type View = "multiplayer" | "choice" | "tournament";
 
@@ -7,6 +9,7 @@ type Props = {
 };
 
 export const GameMode: React.FC<Props> = ({ onSelectMode }) => {
+  useLanguage();
   const { setMode } = useGame();
 
   const handleModeChange = (newMode: "multiplayer" | "tournament") => {
@@ -16,13 +19,13 @@ export const GameMode: React.FC<Props> = ({ onSelectMode }) => {
   return (
     <div className="h-full flex flex-col items-center justify-center m-10">
       <h2 className="text-6xl font-bold mt-20 text-[#2c3781] dark:text-[#cad3f5]">
-        How do you want to play?
+        {t().howDoYouWantToPlay}
       </h2>
       <div className="h-full flex justify-center mt-15">
         <div className="flex-col items-center justify-center">
           <button onClick={() => handleModeChange("multiplayer") } className="mr-10 min-w-50 bg-[#6688cc] hover:bg-[#24273a] rounded-2xl px-6 py-4">
             <p className="text-2xl md:text-4xl text-white">
-              1 vs 1
+              {t().oneVsOne}
             </p>
           </button>
         </div>
@@ -30,7 +33,7 @@ export const GameMode: React.FC<Props> = ({ onSelectMode }) => {
         <div className="flex-col items-center justify-center">
           <button onClick={() => handleModeChange("tournament")} className="ml-10 min-w-50 bg-[#6688cc] hover:bg-[#24273a] rounded-2xl px-6 py-4">
             <p className="text-2xl md:text-4xl text-white">
-              Tournament
+              {t().tournament}
             </p>
           </button>
         </div>
--- frontend/src/components/TournamentSetup.tsx
+++ frontend/src/components/TournamentSetup.tsx
@@ -3,6 +3,8 @@ import { Login } from "./Login"
 import { SignUp } from "./SignUp";
 import { useGame } from "./GameContext";
 import { useAuth } from "./GetAuth";
+import { t } from "./lang";
+import { useLanguage } from "./useLanguage";
 
 type View = "register" | "choice" | "login" | "multiplayer" | "gamemode" |"tournament";
 
@@ -14,6 +16,7 @@ type Props = {
 type Form = "unknown" | "signup" | "login"
 
 export const TournamentSetup: React.FC<Props> = ({ onBack, onSelectMode }) => {
+  useLanguage();
   const { currentPlayerIndex, totalPlayers } = useGame();
   const { login } = useAuth();
   const [form, setForm] = useState<Form>("unknown");
@@ -25,12 +28,12 @@ export const TournamentSetup: React.FC<Props> = ({ onBack, onSelectMode }) => {
   if (form === "unknown") {
     return (
     <div className="min-h-full flex flex-col justify-center items-center p-6">
-      <h1 className="text-3xl font-extrabold text-[#6688cc] dark:text-[#cad3f5] mb-6">Login or Signup Player {currentPlayerIndex + 1} / {totalPlayers}</h1>
-      <div className="flec justify-center items-center"> 
-        <button onClick={() => handleForm("signup")} className="text-white m-5 min-w-50 bg-[#6688cc] hover:bg-[#24273a] rounded-2xl px-6 py-4">SignUp</button>
-        <button onClick={() => handleForm("login")} className="text-white m-5 min-w-50 bg-[#6688cc] hover:bg-[#24273a] rounded-2xl px-6 py-4">LogIn</button>
+      <h1 className="text-3xl font-extrabold text-[#6688cc] dark:text-[#cad3f5] mb-6">{t().loginOrSignupPlayer} {currentPlayerIndex + 1} / {totalPlayers}</h1>
+      <div className="flec justify-center items-center">
+        <button onClick={() => handleForm("signup")} className="text-white m-5 min-w-50 bg-[#6688cc] hover:bg-[#24273a] rounded-2xl px-6 py-4">{t().signUp}</button>
+        <button onClick={() => handleForm("login")} className="text-white m-5 min-w-50 bg-[#6688cc] hover:bg-[#24273a] rounded-2xl px-6 py-4">{t().logIn}</button>
       </div>
-      <button onClick={ onBack } className="m-5 text-sm text-gray-500 dark:text-[#cad3f5] hover:text-gray-700">Back</button>
+      <button onClick={ onBack } className="m-5 text-sm text-gray-500 dark:text-[#cad3f5] hover:text-gray-700">{t().back}</button>
     </div>
     )
   }
