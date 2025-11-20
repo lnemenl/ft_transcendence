3fc0573b89b469354f318a08b2b15ccfe4c60b7a
Colin <cpireyre@student.42.fr>
2025-11-20 10:35:36 +0200

    Dynamically optimise background rendering for best game performance

--- frontend/src/components/Blobs.tsx
+++ frontend/src/components/Blobs.tsx
@@ -1,12 +1,17 @@
 import "../App.css";
 
-export function Blobs() {
+type BlobsProps = {
+	gameActive?: boolean;
+};
+
+export function Blobs({ gameActive = false }: BlobsProps) {
 	return (
 	<div className="fixed inset-0 -z-10 w-screen h-screen overflow-hidden">
 		<svg xmlns="https://www.w3.org/2000/svg" className="absolute pointer-evets-none opacity-0" width="0" height="0" goo-defs="true" aria-hidden="true">
       	<defs>
         	<filter id="goo" x="-50%" y="-50%" width="200%" height="200%" colorInterpolationFilters="sRGB">
-          		<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -18" result="mask" />
+          		{!gameActive && <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />}
+          		{!gameActive && <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -18" result="mask" />}
           		<feComposite in="SourceGraphic" in2="goo" operator="atop" />
         	</filter>
       	</defs>
