178e4b9410f5de27b34f7252d04fea79dff95d02
Colin <cpireyre@student.42.fr>
2025-11-20 09:28:25 +0200

    Optimise background rendering for game performance on Firefox

--- frontend/src/components/Blobs.tsx
+++ frontend/src/components/Blobs.tsx
@@ -6,7 +6,6 @@ export function Blobs() {
 		<svg xmlns="https://www.w3.org/2000/svg" className="absolute pointer-evets-none opacity-0" width="0" height="0" goo-defs="true" aria-hidden="true">
       	<defs>
         	<filter id="goo" x="-50%" y="-50%" width="200%" height="200%" colorInterpolationFilters="sRGB">
-          		<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
           		<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -18" result="mask" />
           		<feComposite in="SourceGraphic" in2="goo" operator="atop" />
         	</filter>
